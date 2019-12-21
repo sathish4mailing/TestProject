ad2a8319b9878f24a9888cb8cafb4e84aa89f757
sathish4mailing <sathish4mailing@yahoo.co.in>
2019-12-21 18:04:14 +0530

    Eclipse & Git Practice - 1

--- GitEclipsePrac/src/test.java
+++ GitEclipsePrac/src/test.java
@@ -3,6 +3,7 @@ import java.util.logging.Logger;
 public class test {
 
 	private static Logger logger = null;
+	
 	public static void main(String[] args) {
 		
 		logger = Logger.getLogger(test.class.getName());
